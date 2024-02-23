8de02b4c794f7033bc26c60b00c87ca650379e76
SalC124 <saltcal124@gmail.com>
2024-02-22 21:03:04 -0500

    Stupid Int Overflow

--- PasswordSSHTest.java
+++ PasswordSSHTest.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 public class PasswordSSHTest {
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
-            System.out.println("What is your userame?");
+            System.out.println("[Account Setup]");
+            System.out.print("Username: ");
             String username = input.nextLine();
-            System.out.println("What is your password?");
+            System.out.print("Password: ");
             String password = input.nextLine();
             SSHer userInfo = new SSHer(username, password);
-            System.out.println("Your username is " + userInfo.getUsername() + " and your password is " + userInfo.SSH());
+            System.out.println("Your username is " + 
+                userInfo.getUsername() + 
+                " and your password is " + userInfo.SSH());
         }
     }
 }
--- SSHer.java
+++ SSHer.java
@@ -1,17 +1,14 @@
+import java.lang.Math;
 public class SSHer {
     // instance variables
     private String username;
     private String password;
+    private int multiplier = (int)(Math.random() * 0) + 10;
 
     // constructor
     public SSHer(String username, String password) {
         this.username = username;
         this.password = password;
-    }
-
-    // toString method
-    public String toString() {
-        return "Username: " + username + "\nPassword: " + password;
     }
 
     // getters
@@ -24,7 +21,7 @@ public class SSHer {
     }
 
     // mutators
-    public String SSH() {
+    public int SSH() {
         int i = 0;
         while(i < password.length()) {
 
@@ -110,85 +107,144 @@ public class SSHer {
 
             // Uppercase
             if (password.charAt(i) == 'A') {
-                password = password.substring(0, i) + "11" + password.substring(i + 1);
+                password = password.substring(0, i) + "27" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'B') {
-                password = password.substring(0, i) + "22" + password.substring(i + 1);
+                password = password.substring(0, i) + "28" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'C') {
-                password = password.substring(0, i) + "33" + password.substring(i + 1);
+                password = password.substring(0, i) + "29" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'D') {
-                password = password.substring(0, i) + "44" + password.substring(i + 1);
+                password = password.substring(0, i) + "30" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'E') {
-                password = password.substring(0, i) + "55" + password.substring(i + 1);
+                password = password.substring(0, i) + "31" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'F') {
-                password = password.substring(0, i) + "66" + password.substring(i + 1);
+                password = password.substring(0, i) + "32" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'G') {
-                password = password.substring(0, i) + "77" + password.substring(i + 1);
+                password = password.substring(0, i) + "33" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'H') {
-                password = password.substring(0, i) + "88" + password.substring(i + 1);
+                password = password.substring(0, i) + "34" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'I') {
-                password = password.substring(0, i) + "99" + password.substring(i + 1);
+                password = password.substring(0, i) + "35" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'J') {
-                password = password.substring(0, i) + "1010" + password.substring(i + 1);
+                password = password.substring(0, i) + "36" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'K') {
-                password = password.substring(0, i) + "1111" + password.substring(i + 1);
+                password = password.substring(0, i) + "37" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'L') {
-                password = password.substring(0, i) + "1212" + password.substring(i + 1);
+                password = password.substring(0, i) + "38" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'M') {
-                password = password.substring(0, i) + "1313" + password.substring(i + 1);
+                password = password.substring(0, i) + "39" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'N') {
-                password = password.substring(0, i) + "1414" + password.substring(i + 1);
+                password = password.substring(0, i) + "40" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'O') {
-                password = password.substring(0, i) + "1515" + password.substring(i + 1);
+                password = password.substring(0, i) + "41" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'P') {
-                password = password.substring(0, i) + "1616" + password.substring(i + 1);
+                password = password.substring(0, i) + "42" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'Q') {
-                password = password.substring(0, i) + "1717" + password.substring(i + 1);
+                password = password.substring(0, i) + "43" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'R') {
-                password = password.substring(0, i) + "1818" + password.substring(i + 1);
+                password = password.substring(0, i) + "44" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'S') {
-                password = password.substring(0, i) + "1919" + password.substring(i + 1);
+                password = password.substring(0, i) + "45" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'T') {
-                password = password.substring(0, i) + "2020" + password.substring(i + 1);
+                password = password.substring(0, i) + "46" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'U') {
-                password = password.substring(0, i) + "2121" + password.substring(i + 1);
+                password = password.substring(0, i) + "47" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'V') {
-                password = password.substring(0, i) + "2222" + password.substring(i + 1);
+                password = password.substring(0, i) + "48" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'W') {
-                password = password.substring(0, i) + "2323" + password.substring(i + 1);
+                password = password.substring(0, i) + "49" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'X') {
-                password = password.substring(0, i) + "2424" + password.substring(i + 1);
+                password = password.substring(0, i) + "50" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'Y') {
-                password = password.substring(0, i) + "2525" + password.substring(i + 1);
+                password = password.substring(0, i) + "51" + password.substring(i + 1);
             }
             if (password.charAt(i) == 'Z') {
-                password = password.substring(0, i) + "2626" + password.substring(i + 1);
+                password = password.substring(0, i) + "52" + password.substring(i + 1);
+            }
+
+            // Symbols
+            if (password.charAt(i) == '!') {
+                password = password.substring(0, i) + "53" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '@') {
+                password = password.substring(0, i) + "54" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '#') {
+                password = password.substring(0, i) + "55" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '$') {
+                password = password.substring(0, i) + "56" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '%') {
+                password = password.substring(0, i) + "57" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '^') {
+                password = password.substring(0, i) + "58" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '&') {
+                password = password.substring(0, i) + "59" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '*') {
+                password = password.substring(0, i) + "60" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '(') {
+                password = password.substring(0, i) + "61" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == ')') {
+                password = password.substring(0, i) + "62" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '[') {
+                password = password.substring(0, i) + "61" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == ']') {
+                password = password.substring(0, i) + "62" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '{') {
+                password = password.substring(0, i) + "63" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '}') {
+                password = password.substring(0, i) + "64" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '-') {
+                password = password.substring(0, i) + "65" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '+') {
+                password = password.substring(0, i) + "66" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '_') {
+                password = password.substring(0, i) + "67" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '=') {
+                password = password.substring(0, i) + "68" + password.substring(i + 1);
+            }
+            if (password.charAt(i) == '?') {
+                password = password.substring(0, i) + "69" + password.substring(i + 1);
             }
             i++;
         }
-        return password;
+        return (Integer.parseInt(password) * multiplier);
     }
 }
